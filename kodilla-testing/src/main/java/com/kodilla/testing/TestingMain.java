dfc8c30d1dc4523b062461f0aca8bf8351c03022
MichalGubera <m.gubera@gmail.com>
2018-02-07 18:20:46 +0100

    add class simpleUser and calculator with tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addition = 7;
+        int substraction = 3;
+
+        if(calculator.add(4,3) == addition){
+            System.out.println("addition test OK");
+        } else {
+            System.out.println("addition test failed!");
+        }
+
+        if(calculator.substract(7,4) == substraction){
+            System.out.println("substraction test OK");
+        } else {
+          System.out.println("substraction test failed!");
+        }
     }
 }
