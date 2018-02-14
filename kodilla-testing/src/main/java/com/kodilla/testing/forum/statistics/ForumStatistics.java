95c0813fd88e9f500e5835765573332f223679b3
MichalGubera <m.gubera@gmail.com>
2018-02-14 12:36:14 +0100

    fix ShapeCollectorTestSuite.java

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -1,4 +1,13 @@
 package com.kodilla.testing.forum.statistics;
 
+import java.util.List;
+
 public class ForumStatistics {
+
+    public void calculateAdvStatistics(){
+        // obliczy podane powyżej wartości i zapamięta je we właściwościach (polach) klasy
+    }
+    public void ShowStatistics(){
+        // wyświetli zapamiętane we właściwościach statystyki
+    }
 }
