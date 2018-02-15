2690db2e77c7f1a40d6d5053cc31f4837a124e5b
MichalGubera <m.gubera@gmail.com>
2018-02-15 12:16:30 +0100

    add test

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -1,18 +1,31 @@
 package com.kodilla.testing.forum.statistics;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ForumStatisticsTestSuite {
     @Test
     public void testCalculateAdvStatistics(){
         //Given
         Statistics statisticsMock = mock(Statistics.class);
-        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
-        //When
-        int postsCount = forumStatistics.calculateAdvStatistics();
-        //Then
+        List<String> usersNamesList = new ArrayList<String>();
+        usersNamesList.add("Joe");
+        usersNamesList.add("Mick");
+        usersNamesList.add("Paul");
+        usersNamesList.add("Topper");
+        when(statisticsMock.usersNames()).thenReturn(usersNamesList);
 
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+
+        //When
+        int usersCount = forumStatistics.calculateAdvStatistics(statisticsMock);
+        //Then
+        Assert.assertEquals(4, usersCount);
     }
 }
