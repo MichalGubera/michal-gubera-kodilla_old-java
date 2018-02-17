396b1f69a12d1a547477b90b9f09ebb906c78a9a
MichalGubera <m.gubera@gmail.com>
2018-02-17 21:36:24 +0100

    skip new object in PoemBeautifierExecutive

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifierExecutive.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifierExecutive.java
@@ -2,27 +2,23 @@ package com.kodilla.stream.beautifier;
 
 public class PoemBeautifierExecutive {
     public static String addBeginAndEnd(String string){
-        String stringToBeautify = new String(string);
         String begin = "Uwaga! Zaczynam... : ";
-        String beginWith = begin.concat(stringToBeautify);
+        String beginWith = begin.concat(string);
         return beginWith.concat(" - I co?!");
     }
 
     public static String toUpper(String string){
-        String stringToBeautify = new String(string);
-        return stringToBeautify.toUpperCase();
+        return string.toUpperCase();
     }
 
     public static String toLower(String string){
-        String stringToBeautify = new String(string);
-        return stringToBeautify.toLowerCase();
+        return string.toLowerCase();
     }
 
     public static String reverse(String string){
-        String stringToBeautify = new String(string);
         String reversed = "";
-        for(int i=(stringToBeautify.length())-1; i>0; i-- ){
-            reversed += stringToBeautify.charAt(i-1);
+        for(int i=(string.length())-1; i>0; i-- ){
+            reversed += string.charAt(i-1);
         }
         return reversed;
     }
