b60ec71d9001ba34c7fa4ba3daa8beb0c555c0e0
MichalGubera <m.gubera@gmail.com>
2018-02-17 22:13:30 +0100

    add 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemBeautifierExecutive;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -13,5 +14,9 @@ public class StreamMain {
         poemBeautifier.beautify("I spójrzałem na pole; dwieście armat grzmiało.", PoemBeautifierExecutive :: toUpper);
         poemBeautifier.beautify("Artyleryi ruskiej ciągną się szeregi", PoemBeautifierExecutive :: toLower);
         poemBeautifier.beautify("Prosto, długo, daleko, jako morza brzegi;.", PoemBeautifierExecutive :: reverse);
+
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
