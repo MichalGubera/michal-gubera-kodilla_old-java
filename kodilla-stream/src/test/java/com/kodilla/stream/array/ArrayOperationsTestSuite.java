d4776b7067729dfd776ee9a37e4dcbfdeab5565c
MichalGubera <m.gubera@gmail.com>
2018-02-24 12:50:41 +0100

    add excercise 7.5

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -2,6 +2,7 @@ package com.kodilla.stream.array;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import static com.kodilla.stream.array.ArrayOperations.getAverage;
 
 public class ArrayOperationsTestSuite {
